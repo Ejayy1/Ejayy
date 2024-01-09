3b8b99567c943e5fe0900d44f726e52c5ffc5f24
Ejayy1 <156073240+Ejayy1@users.noreply.github.com>
2024-01-09 17:52:18 +0800

    Add files via upload

---
+++ HangMan Game For CC106/scripts/word-list.js
@@ -0,0 +1,111 @@
+const wordList = [
+    {
+        word: "container tag",
+        hint: "Have the start tag and end tags together with the content."
+    },
+    {
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life."
+    },
+    {
+        word: "empty tag",
+        hint: "A stand alone and do not require end tags."
+    },
+    {
+        word: "javascript variable",
+        hint: "It acts as a container that holds data that can be used and manipulated throughout a program."
+    },
+    {
+        word: "javascript",
+        hint: "It is a popular programming language that has a wide range of applications. It was previously used mainly for making webpages interactive such as form validation, animation, etc."
+    },
+    {
+        word: "external style sheets",
+        hint: "These are the most global of the three kinds of CSS because you can apply the same one to an unlimited number of pages."
+    },
+    {
+        word: "embedded style sheets",
+        hint: "These are used for creating document wide style rule. They are placed within an HTML document between the <head> and </head>"
+    },
+    {
+        word: "inline style sheets",
+        hint: "These are used for isolated changes to headline, paragraph, picture or other element."
+    },
+    {
+        word: "metadata",
+        hint: "The information or data about data. It is about the HTML document which is placed in the head part and will not be displayed on the page."
+    },
+    {
+        word: "headings",
+        hint: "Container tags which format text using pre-defined values for size and color."
+    },
+    {
+        word: "notepad",
+        hint: "A simple text editor tool that comes with windows by default."
+    },
+    {
+        word: "markup language",
+        hint: "It is a set of markup tags."
+    },
+    {
+        word: "html",
+        hint: "A markup language for describing web documents."
+    },
+    {
+        word: "severless computing",
+        hint: "This architecture allows developers to build and run applications without managing the underlying infrastructure."
+    },
+    {
+        word: "argumented reality and virtual reality",
+        hint: "It overlays digital content on the real world, while VR creates immersive environments."
+    },
+    {
+        word: "blockchain technology",
+        hint: "Blockchain provides secure and transparent data storage through decentralized ledgers. Beyond cryptocurrencies, it's used in supply chain, finance, and more."
+    },
+    {
+        word: "internet of things",
+        hint: "It connects everyday objects to the internet, allowing them to exchange data and perform tasks. Smart homes, wearable devices, and industrial automation are some examples."
+    },
+    {
+        word: "artificial intelligence and machine learning",
+        hint: "These technologies enable applications to learn from data and make intelligent decisions."
+    },
+    {
+        word: "emerging technologies",
+        hint: "It is used to describe a new technology, but it may also refer to the continuing development of an existing technology."
+    },
+    {
+        word: "evolution",
+        hint: "The process of developing by gradual changes."
+    },
+    {
+        word: "hybrid apps",
+        hint: "Part native apps, part web apps. Like naive apps they live in an app store and can take advantage of the many device features available."
+    },
+    {
+        word: "mobile web apps",
+        hint: "They are not real applications; they are really websites that in many ways look and feel like native applications but are not implemented."
+    },
+    {
+        word: "native apps",
+        hint: "A software application built in a specific programming language for the specific device platform either ios or android."
+    },
+    {
+        word: "mobile application",
+        hint: "The process to making software for smartphones and digital assistants, most commonly for android and ios."
+    },
+    {
+        word: "agile",
+        hint: "The development as a response to growing frustrations with waterfall and other highly structured inflexible methodologies. This approach is designed to accommodate change and the need to produce software faster."
+    },
+    {
+        word: "waterfall",
+        hint: "It is a method of application development is planning and sequence. The entire project is mapped out in the planning analysis stages."
+    },
+    {
+        word: "application development",
+        hint: "Often referred to as software development, is the process of creating software applications to perform specific tasks or solve particular problems."
+    },
+    
+];
